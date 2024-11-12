39f9632ba8e624eb91e840b7c9ee2a9b76f2b413
Jhache06 <jenniferhache06@gmail.com>
2024-11-11 23:32:00 -0500

    Add files via upload

--- JenniferHache_Chapter#11_A.py
+++ JenniferHache_Chapter#11_A.py
@@ -1,60 +1,86 @@
 import random
 
-# create class Deck to represent deck of cards
+# Create class Deck to represent deck of cards
 class Deck:
-    def __init__(self, size):
+    def __init__(self, size=52):
         self.card_list = [i for i in range(size)]
-        # use random function to shuffle cards
+        # Shuffle cards using random method
         random.shuffle(self.card_list)
-        # store current card
         self.current_card = 0
-        # store card deck size of 52
-        self.size = size
 
-    # deal card from deck (self)
+    # Deals a card from the deck
     def deal(self):
-        # use if statement to reshuffle cards if all cards have been dealt
-        if self.size - self.current_card < 1:
+        if self.current_card >= len(self.card_list):
             random.shuffle(self.card_list)
+            # Reshuffle if all cards are dealt
             self.current_card = 0
-            # let user/player know cards are being reshuffled
             print('Reshuffling...!!!')
+        card = self.card_list[self.current_card]
         self.current_card += 1
-        # return current card (self)
-        return self.card_list[self.current_card - 1]
+        return card
 
-# list card ranks
-ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-# list of suits: clubs, diamonds, hearts, spades
-suits = ['clubs', 'diamonds', 'hearts', 'spades']
+    # Deals an initial hand of cards.
+    def deal_initial_hand(self, num_cards=5):
+        hand = []
+        for _ in range(num_cards):
+            card = self.deal()
+            hand.append(card)
+        return hand
 
-# Create a deck of 52 cards
-my_deck = Deck(52)
+    # Replace selected cards in the hand
+    def replace_cards(self, hand, cards_to_replace):
+        for card_index in cards_to_replace:
+            card = self.deal()
+            hand[card_index] = card
+        return hand
 
-# Deal 5 cards for the initial hand
-initial_hand = []
-for i in range(5):
-    # deal 5 cards
-    d = my_deck.deal()
-    r = d % 13
-    # Get rank
-    s = d // 13
-    # Get suit
-    initial_hand.append((r, s))
-    print(ranks[r], 'of', suits[s])
 
-# Ask the user/player for which cards to replace. must choose up to 5 cards max.(1-5)
-cards_to_replace = input("Enter the numbers of the cards you want to replace (Can only choose up to card 5): ")
-cards_to_replace = [int(x) - 1 for x in cards_to_replace.split(',')]
+# List of card ranks and suits
+# Returns the rank a suit for a given card number.
+def get_card_info(card):
+    ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = ['clubs', 'diamonds', 'hearts', 'spades']
+    rank = card % 13
+    suit = card // 13
+    return ranks[rank], suits[suit]
 
-# replace selected cards
-for card_index in cards_to_replace:
-    d = my_deck.deal()
-    r = d % 13
-    s = d // 13
-    initial_hand[card_index] = (r, s)
 
-# print the final hand after replacing cards. let user know this is the final hand
-print("\nFinal hand after draw:")
-for r, s in initial_hand:
-    print(ranks[r], 'of', suits[s])
+# Prints the current hand
+def print_hand(hand):
+    for card in hand:
+        rank, suit = get_card_info(card)
+        print(f'{rank} of {suit}')
+
+
+def play_game():
+    # Create a deck of cards
+    deck = Deck()
+
+    # Deal an initial hand of 5 cards
+    initial_hand = deck.deal_initial_hand()
+    print("Initial hand:")
+    print_hand(initial_hand)
+
+    # Ask the user/player for which cards to replace (1-5)
+    try:
+        cards_to_replace = input("Enter the numbers of the cards you want to replace (Any card from 1-5): ")
+        cards_to_replace = [int(x.strip()) - 1 for x in cards_to_replace.split(',')]
+        # Convert to 0-based index
+        if any(card < 0 or card > 4 for card in cards_to_replace):
+            # If user enters anything other that 1-5, error message will display.
+            raise ValueError("Error! Please select cards between 1 and 5.")
+    except ValueError as e:
+        print(f"Invalid input: {e}")
+        return  # Exit if input is invalid
+
+    # Replace selected cards
+    updated_hand = deck.replace_cards(initial_hand, cards_to_replace)
+
+    # Print the final hand after replacing cards
+    print("\nFinal hand:")
+    print_hand(updated_hand)
+
+
+# Start the game
+if __name__ == "__main__":
+    play_game()
